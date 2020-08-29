27cf4001d00e3e4de29c107fdc3a6627285340b5
Eli Polonsky <epolon@amazon.com>
2020-08-29 12:49:32 +0300

    mid work

--- integ-tests/integ.py
+++ integ-tests/integ.py
@@ -10,42 +10,18 @@ stack = cdk.Stack(app, 'aws-cdk-lambdecor-integ-test')
 bucket = s3.Bucket(stack, 'Bucket', removal_policy=cdk.RemovalPolicy.DESTROY)
 
 @aws_lambda(stack)
-def return_string(arg):
-  return f'arg={arg}'
-
-@aws_lambda(stack)
-def return_int(arg):
-  return arg + 1
-
-@aws_lambda(stack)
-def return_boolean(arg):
-  return arg != True
-
-@aws_lambda(stack)
-def return_list(arg):
-  return [arg]
-
-@aws_lambda(stack)
-def return_dictionary(arg):
-  return {'arg': arg}
-
-@aws_lambda(stack)
-def accept_token(arg):
-  return f'token={arg}'
-
-@aws_lambda(stack)
-def typeof(arg):
-  return f'({arg})({type(arg)})'
+def typeof(positional, named):
+  return f'(positional={positional})({type(positional)}) | (named={named})({type(named)})'
 
 def make_output(name, value):
   cdk.CfnOutput(stack, name, value=value)
 
-make_output('String', typeof('input'))
-make_output('Integer', typeof(5))
-make_output('Boolean', typeof(True))
+make_output('String', typeof('input', named='input'))
+make_output('Integer', typeof(5, named=5))
+make_output('Boolean', typeof(True, named=True))
 # make_output('List', return_list('input'))
 # make_output('Dictionary', return_dictionary('input'))
-make_output('TokenString', typeof(bucket.bucket_name))
+make_output('TokenString', typeof(bucket.bucket_name, named=bucket.bucket_name))
 
 app.synth()
 
